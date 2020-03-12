f94ecbfeb46f2329e1a034692c0f40021f6612fa
rupeshubhagat <bhagat.r29@gmail.com>
2020-03-12 20:15:30 +0530

    Working on contact page install multiselted plugin

--- src/app/layout/index/contactus/contactus.component.ts
+++ src/app/layout/index/contactus/contactus.component.ts
@@ -9,7 +9,263 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class ContactusComponent implements OnInit {
   sign:FormGroup;
   frmSign:object;
-  inquiryTypes: any = ['Request For Services', 'Alliances', 'Alumni', 'Investor Relations','Media']
+  inquiryTypes: any = ['Request For Services', 'Alliances', 'Alumni', 'Investor Relations','Media'];
+  countryList:any = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas (the)",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bermuda",
+    "Bhutan",
+    "Bolivia (Plurinational State of)",
+    "Bonaire, Sint Eustatius and Saba",
+    "Bosnia and Herzegovina",
+    "Botswana",
+    "Bouvet Island",
+    "Brazil",
+    "British Indian Ocean Territory (the)",
+    "Brunei Darussalam",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cabo Verde",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "Cayman Islands (the)",
+    "Central African Republic (the)",
+    "Chad",
+    "Chile",
+    "China",
+    "Christmas Island",
+    "Cocos (Keeling) Islands (the)",
+    "Colombia",
+    "Comoros (the)",
+    "Congo (the Democratic Republic of the)",
+    "Congo (the)",
+    "Cook Islands (the)",
+    "Costa Rica",
+    "Croatia",
+    "Cuba",
+    "Curaçao",
+    "Cyprus",
+    "Czechia",
+    "Côte d'Ivoire",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic (the)",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Eswatini",
+    "Ethiopia",
+    "Falkland Islands (the) [Malvinas]",
+    "Faroe Islands (the)",
+    "Fiji",
+    "Finland",
+    "France",
+    "French Guiana",
+    "French Polynesia",
+    "French Southern Territories (the)",
+    "Gabon",
+    "Gambia (the)",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Gibraltar",
+    "Greece",
+    "Greenland",
+    "Grenada",
+    "Guadeloupe",
+    "Guam",
+    "Guatemala",
+    "Guernsey",
+    "Guinea",
+    "Guinea-Bissau",
+    "Guyana",
+    "Haiti",
+    "Heard Island and McDonald Islands",
+    "Holy See (the)",
+    "Honduras",
+    "Hong Kong",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran (Islamic Republic of)",
+    "Iraq",
+    "Ireland",
+    "Isle of Man",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jersey",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kiribati",
+    "Korea (the Democratic People's Republic of)",
+    "Korea (the Republic of)",
+    "Kuwait",
+    "Kyrgyzstan",
+    "Lao People's Democratic Republic (the)",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Macao",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Marshall Islands (the)",
+    "Martinique",
+    "Mauritania",
+    "Mauritius",
+    "Mayotte",
+    "Mexico",
+    "Micronesia (Federated States of)",
+    "Moldova (the Republic of)",
+    "Monaco",
+    "Mongolia",
+    "Montenegro",
+    "Montserrat",
+    "Morocco",
+    "Mozambique",
+    "Myanmar",
+    "Namibia",
+    "Nauru",
+    "Nepal",
+    "Netherlands (the)",
+    "New Caledonia",
+    "New Zealand",
+    "Nicaragua",
+    "Niger (the)",
+    "Nigeria",
+    "Niue",
+    "Norfolk Island",
+    "Northern Mariana Islands (the)",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palau",
+    "Palestine, State of",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines (the)",
+    "Pitcairn",
+    "Poland",
+    "Portugal",
+    "Puerto Rico",
+    "Qatar",
+    "Republic of North Macedonia",
+    "Romania",
+    "Russian Federation (the)",
+    "Rwanda",
+    "Réunion",
+    "Saint Barthélemy",
+    "Saint Helena, Ascension and Tristan da Cunha",
+    "Saint Kitts and Nevis",
+    "Saint Lucia",
+    "Saint Martin (French part)",
+    "Saint Pierre and Miquelon",
+    "Saint Vincent and the Grenadines",
+    "Samoa",
+    "San Marino",
+    "Sao Tome and Principe",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Sint Maarten (Dutch part)",
+    "Slovakia",
+    "Slovenia",
+    "Solomon Islands",
+    "Somalia",
+    "South Africa",
+    "South Georgia and the South Sandwich Islands",
+    "South Sudan",
+    "Spain",
+    "Sri Lanka",
+    "Sudan (the)",
+    "Suriname",
+    "Svalbard and Jan Mayen",
+    "Sweden",
+    "Switzerland",
+    "Syrian Arab Republic",
+    "Taiwan (Province of China)",
+    "Tajikistan",
+    "Tanzania, United Republic of",
+    "Thailand",
+    "Timor-Leste",
+    "Togo",
+    "Tokelau",
+    "Tonga",
+    "Trinidad and Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Turks and Caicos Islands (the)",
+    "Tuvalu",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates (the)",
+    "United Kingdom of Great Britain and Northern Ireland (the)",
+    "United States Minor Outlying Islands (the)",
+    "United States of America (the)",
+    "Uruguay",
+    "Uzbekistan",
+    "Vanuatu",
+    "Venezuela (Bolivarian Republic of)",
+    "Viet Nam",
+    "Virgin Islands (British)",
+    "Virgin Islands (U.S.)",
+    "Wallis and Futuna",
+    "Western Sahara",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe",
+    "Åland Islands"
+  ];
+  requestType:any;
+  showServices:boolean;
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
   constructor() { }
   ngOnInit() {
     this.sign = new FormGroup({
@@ -22,11 +278,72 @@ export class ContactusComponent implements OnInit {
       services: new FormControl(""),
       message: new FormControl("")
     });
+    // for multiselect coding
+    this.dropdownList = [
+      {"id":1,"itemName":"India"},
+      {"id":2,"itemName":"Singapore"},
+      {"id":3,"itemName":"Australia"},
+      {"id":4,"itemName":"Canada"},
+      {"id":5,"itemName":"South Korea"},
+      {"id":6,"itemName":"Germany"},
+      {"id":7,"itemName":"France"},
+      {"id":8,"itemName":"Russia"},
+      {"id":9,"itemName":"Italy"},
+      {"id":10,"itemName":"Sweden"}
+    ];
+this.selectedItems = [
+       
+    ];
+this.dropdownSettings = { 
+          singleSelection: false, 
+          text:"SELECT HERE",
+          selectAllText:'Select All',
+          unSelectAllText:'UnSelect All',
+          enableSearchFilter: true,
+          classes:"myclass custom-class"
+        };
+    // multiselect end
+  } // ngOnint
+  onItemSelect(item:any){
+    console.log(item);
+    console.log(this.selectedItems);
+}
+OnItemDeSelect(item:any){
+    console.log(item);
+    console.log(this.selectedItems);
+}
+onSelectAll(items: any){
+    console.log(items);
+}
+onDeSelectAll(items: any){
+    console.log(items);
+}
+
+  setRequest(item){
+      this.requestType = item;
+      console.log(this.requestType);
+      if(this.requestType == "Request For Services")
+      {
+       this.showServices=true;
+       console.log(this.showServices);
+      }
+      else{
+        this.showServices=false;
+      }
+
   }
+
 
   register(sign){
    this.frmSign = {
-     name : sign.name
+     inquiry: sign.inquiry,
+     name : sign.name,
+     email : sign.email,
+     contact : sign.contact,
+     orgnization :sign.orgnization,
+     country :sign.country,
+     services :sign.services,
+     message :sign.message
    }
    console.log(this.frmSign);
    alert("Working");
--- src/app/layout/index/index.module.ts
+++ src/app/layout/index/index.module.ts
@@ -14,6 +14,7 @@ import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, IndexComponent, FulljsComponent, PagenotfoundComponent, ContactusComponent],
@@ -25,7 +26,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     CarouselModule,
     AngularFullpageModule,
     ScrollToModule.forRoot(),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularMultiSelectModule
   ],
   exports: [HeaderComponent, FooterComponent, IndexComponent, FulljsComponent,PagenotfoundComponent,ContactusComponent]
 })
